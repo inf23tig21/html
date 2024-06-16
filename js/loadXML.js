b8b7c6da09f16accb8cda124fbe83ec49a2d84ab
A044351 <160946236+AmFilipe@users.noreply.github.com>
2024-06-16 21:28:28 +0100

    Update loadXML.js

--- js/loadXML.js
+++ js/loadXML.js
@@ -5,43 +5,30 @@ function loadXMLDoc(filename) {
     } else {
         xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xhttp.open("GET", filename, true);
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            var xmlDoc = xhttp.responseXML;
-            if (xmlDoc) {
-                console.log("XML carregado com sucesso");
-                processXML(xmlDoc);
-            } else {
-                console.error("Erro ao carregar XML");
-            }
-        } else if (xhttp.readyState == 4) {
-            console.error("Erro na solicitação: " + xhttp.status);
-        }
-    };
+    xhttp.open("GET", filename, false);
     xhttp.send();
-}
+    var xmlDoc = xhttp.responseXML;
 
-function processXML(xmlDoc) {
+    // Obter todos os elementos <List> do XML
     var listElements = xmlDoc.getElementsByTagName("List");
-    console.log("Número de elementos <List> encontrados: " + listElements.length);
+
+    // Iterar sobre cada elemento <List> no XML
     for (var i = 0; i < listElements.length; i++) {
         var list = listElements[i];
         var nome = list.getElementsByTagName("Nome")[0].textContent;
         var localizacao = list.getElementsByTagName("Localizacao")[0].textContent;
         var horarioSemana = list.getElementsByTagName("HorarioAberturaSemana")[0].textContent + " - " + list.getElementsByTagName("HorarioFechoSemana")[0].textContent;
         var horarioFimSemana = list.getElementsByTagName("HorarioAberturaFimSemana")[0].textContent + " - " + list.getElementsByTagName("HorarioFechoFimSemana")[0].textContent;
-        var tipo = list.getElementsByTagName("Tipo")[0].textContent;
 
-        console.log("Processando elemento:", nome, localizacao, horarioSemana, horarioFimSemana, tipo);
-
+        // Construir a string com os detalhes do campo
         var detalhesCampo = "<div class='campo'>" +
                             "<h4>" + nome + "</h4>" +
-                            "<p><strong>Localização:</strong> " + localizacao + "</p>" +
-                            "<p><strong>Horário Semana:</strong> " + horarioSemana + "</p>" +
-                            "<p><strong>Horário Fim de Semana:</strong> " + horarioFimSemana + "</p>" +
+                            "<p><strong>LocalizaÃ§Ã£o:</strong> " + localizacao + "</p>" +
+                            "<p><strong>HorÃ¡rio Semana:</strong> " + horarioSemana + "</p>" +
+                            "<p><strong>HorÃ¡rio Fim de Semana:</strong> " + horarioFimSemana + "</p>" +
                             "</div>";
 
+        // Adicionar os detalhes do campo Ã  seÃ§Ã£o apropriada
         var sectionId;
         switch (tipo) {
             case "Futebol":
@@ -54,19 +41,12 @@ function processXML(xmlDoc) {
                 sectionId = "campos-tenis";
                 break;
             default:
-                console.warn("Tipo desconhecido:", tipo);
                 continue; // Ignorar tipos desconhecidos
         }
 
-        var section = document.querySelector(".campo-section#" + sectionId);
+        var section = document.getElementById(sectionId);
         if (section) {
             section.innerHTML += detalhesCampo;
-            console.log("Adicionado à seção:", sectionId);
-        } else {
-            console.error("Seção não encontrada para o tipo:", tipo);
         }
     }
 }
-
-// Chame a função com o nome do arquivo XML
-loadXMLDoc('xml/reserva_campo1.xml');
