2d756cb1ebcb78a8ff6cbe0e372d82252f2d14c9
A044351 <160946236+AmFilipe@users.noreply.github.com>
2024-06-16 17:49:13 +0100

    Update loadXML.js

--- js/loadXML.js
+++ js/loadXML.js
@@ -1,74 +1,57 @@
-// Função para carregar e processar o XML
 function loadXMLDoc(filename) {
     var xhttp;
     if (window.XMLHttpRequest) {
-        // Para navegadores modernos
         xhttp = new XMLHttpRequest();
     } else {
-        // Para versões antigas do Internet Explorer
         xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     xhttp.open("GET", filename, false);
     xhttp.send();
-    return xhttp.responseXML;
-}
+    var xmlDoc = xhttp.responseXML;
 
-// Função para extrair e exibir os dados do XML
-function displayXML() {
-    var xmlDoc = loadXMLDoc("../xml/reserva_campo1.XML");
+    // Obter todos os elementos <List> do XML
+    var listElements = xmlDoc.getElementsByTagName("List");
 
-    // Acessando os elementos do XML
-    var lists = xmlDoc.getElementsByTagName("List");
+    // Criar a tabela HTML
+    var table = document.createElement("table");
+    table.border = "1";
 
-    // Criando elementos HTML para exibir os dados
-    var section = document.querySelector("main section");
-    var heading = document.createElement("h3");
-    heading.textContent = "Lista de Campos Disponíveis";
-
-    section.appendChild(heading);
-
-    // Iterando sobre cada item da lista no XML
-    for (var i = 0; i < lists.length; i++) {
-        var nome = lists[i].getElementsByTagName("Nome")[0].textContent;
-        var localizacao = lists[i].getElementsByTagName("Localizacao")[0].textContent;
-        var tipo = lists[i].getElementsByTagName("Tipo")[0].textContent;
-        var diaSemana = lists[i].getElementsByTagName("Dia")[0].textContent;
-        var horarioSemana = lists[i].getElementsByTagName("HorarioAberturaSemana")[0].textContent + " - " + lists[i].getElementsByTagName("HorarioFechoSemana")[0].textContent;
-        var fimSemana = lists[i].getElementsByTagName("FimSemana")[0].textContent;
-        var horarioFimSemana = lists[i].getElementsByTagName("HorarioAberturaFimSemana")[0].textContent + " - " + lists[i].getElementsByTagName("HorarioFechoFimSemana")[0].textContent;
-
-        // Criando elementos HTML para cada campo
-        var divCampo = document.createElement("div");
-        divCampo.classList.add("campo");
-
-        var tituloCampo = document.createElement("h4");
-        tituloCampo.textContent = nome;
-
-        var tipoCampo = document.createElement("p");
-        tipoCampo.textContent = "Tipo: " + tipo;
-
-        var infoLocalizacao = document.createElement("p");
-        infoLocalizacao.textContent = "Localização: " + localizacao;
-
-        var infoDiaSemana = document.createElement("p");
-        infoDiaSemana.innerHTML = "<strong>Dias da Semana:</strong><br>" + diaSemana + horarioSemana;
-
-        var infoFimSemana = document.createElement("p");
-        infoFimSemana.innerHTML = "<strong>Fim de Semana:</strong><br>" + fimSemana + horarioFimSemana;
-
-        // Adicionando elementos ao divCampo
-        divCampo.appendChild(tituloCampo);
-        divCampo.appendChild(tipoCampo);
-        divCampo.appendChild(infoLocalizacao);
-        divCampo.appendChild(infoDiaSemana);
-        divCampo.appendChild(infoFimSemana);
-
-        // Adicionando divCampo à seção principal
-        section.appendChild(divCampo);
+    // Cabeçalho da tabela
+    var headerRow = table.createTHead().insertRow();
+    var headers = ["Nome", "Localização", "Tipo", "Dia", "Horário Semana", "Horário Fim de Semana"];
+    for (var h = 0; h < headers.length; h++) {
+        var headerCell = document.createElement("th");
+        headerCell.textContent = headers[h];
+        headerRow.appendChild(headerCell);
     }
-}
 
-// Chamando a função displayXML quando o documento HTML estiver carregado
-document.addEventListener("DOMContentLoaded", function() {
-    displayXML();
-});
+    // Corpo da tabela
+    for (var i = 0; i < listElements.length; i++) {
+        var list = listElements[i];
+        var nome = list.getElementsByTagName("Nome")[0].textContent;
+        var localizacao = list.getElementsByTagName("Localizacao")[0].textContent;
+        var tipo = list.getElementsByTagName("Tipo")[0].textContent;
+        var dia = list.getElementsByTagName("Dia")[0].textContent;
+        var horarioSemana = list.getElementsByTagName("HorarioAberturaSemana")[0].textContent + " - " + list.getElementsByTagName("HorarioFechoSemana")[0].textContent;
+        var horarioFimSemana = list.getElementsByTagName("HorarioAberturaFimSemana")[0].textContent + " - " + list.getElementsByTagName("HorarioFechoFimSemana")[0].textContent;
+
+        var row = table.insertRow();
+        var cell1 = row.insertCell();
+        var cell2 = row.insertCell();
+        var cell3 = row.insertCell();
+        var cell4 = row.insertCell();
+        var cell5 = row.insertCell();
+        var cell6 = row.insertCell();
+
+        cell1.textContent = nome;
+        cell2.textContent = localizacao;
+        cell3.textContent = tipo;
+        cell4.textContent = dia;
+        cell5.textContent = horarioSemana;
+        cell6.textContent = horarioFimSemana;
+    }
+
+    // Adicionar a tabela ao elemento main na página HTML
+    var main = document.querySelector("main");
+    main.appendChild(table);
+}
